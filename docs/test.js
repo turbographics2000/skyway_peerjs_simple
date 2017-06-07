700db26ed6c16d72850f5ebe0868a24390ec98c1
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-08 02:21:22 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -28,7 +28,7 @@ function webCamSetup(elm) {
   }).then(stream => {
     setTimeout(_ => {
       elm.srcObject = stream;
-    }, 2000);
+    }, elm.id === 'selfView' ? 2000 : 0);
     return stream;
   }).catch(ex => console.log('getUserMedia error.', ex));
 }
