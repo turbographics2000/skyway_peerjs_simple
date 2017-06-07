ddc20a70066c0a7db3c7066c2944e9ddaa41f88e
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-07 20:14:42 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -1,150 +1,42 @@
-// var debugLevel = 2;
-// var peer = new Peer({ key: 'ce16d9aa-4119-4097-a8a5-3a5016c6a81c', /*debug: 3*/ });
+var debugLevel = 2;
+var peer = new Peer({ key: 'ce16d9aa-4119-4097-a8a5-3a5016c6a81c', /*debug: 3*/ });
 
-// peer.on('open', id => {
-//   console.log('peer on "open"');
-//   myIdDisp.textContent = id;
-//   btnStart.style.display = '';
-//   btnStart.onclick = evt => {
-//     webCamSetup(selfView).then(stream => {
-//       var call = peer.call(callTo.value, stream);
-//       callSetup(call);
-//     });
-//   }
-// });
-
-// peer.on('call', call => {
-//   console.log('peer on "call"');
-//   webCamSetup(selfView).then(stream => {
-//     call.answer(stream);
-//   });
-//   callSetup(call);
-// });
-
-// function webCamSetup(elm) {
-//   return navigator.mediaDevices.getUserMedia({
-//     video: true,
-//     audio: false
-//   }).then(stream => {
-//     elm.srcObject = stream;
-//     return stream;
-//   }).catch(ex => console.log('getUserMedia error.', ex));
-// }
-
-// function callSetup(call) {
-//   call.on('stream', stream => {
-//     console.log('call on "stream"');
-//     remoteView.srcObject = stream;
-//   });
-//   call.on('close', _ => {
-//     console.log('call on "close"');
-//   });
-// }
-
-
-var apiKey = 'ce16d9aa-4119-4097-a8a5-3a5016c6a81c';
-var token = Math.random().toString(36).substr(2);
-var socket, pc, myId;
-fetch(`https://skyway.io/${apiKey}/id?ts=${Date.now()}${Math.random()}`).then(res => res.text()).then(id => {
-  myIdDisp.textContent = myId = id;
-  socket = new WebSocket(`wss://skyway.io/peerjs?key=${apiKey}&id=${myId}&token=${token}`);
-  socketSetup(socket);
+peer.on('open', id => {
+  console.log('peer on "open"');
+  myIdDisp.textContent = id;
   btnStart.style.display = '';
+  btnStart.onclick = evt => {
+    webCamSetup(selfView).then(stream => {
+      var call = peer.call(callTo.value, stream);
+      callSetup(call);
+    });
+  }
 });
 
-btnStart.onclick = evt => {
-  pcSetup(callTo.value);
+peer.on('call', call => {
+  console.log('peer on "call"');
   webCamSetup(selfView).then(stream => {
-    pc.addStream(stream);
+    call.answer(stream);
   });
-}
+  callSetup(call);
+});
 
-function socketSetup() {
-  socket.onopen = function () {
-    console.log('socket on open');
-  }
-  socket.onmessage = function (evt) {
-    var msg = JSON.parse(evt.data);
-    console.log('msg', JSON.stringify(msg));
-    if (!pc && msg.src) {
-      console.log('pcSetup', 'remoteId:' + msg.src, msg);
-      pcSetup(msg.src);
-    }
-    if (msg.type === 'OFFER') {
-      console.log('%cRecieve offer', 'color: red', msg.ofr);
-      pc.setRemoteDescription(new RTCSessionDescription(msg.ofr))
-        .then(_ => {
-          console.log('%cCreate answer', 'color: red');
-          return pc.createAnswer();
-        })
-        .then(answer => {
-          console.log('%csetLocalDescription(answer)', 'color: red', answer);
-          return pc.setLocalDescription(answer);
-        })
-        .then(_ => {
-          console.log('%cSend answer', 'color: red', 'dst:' + pc.remoteId, pc.localDescription);
-          socket.send(JSON.stringify({
-            type: 'ANSWER',
-            ans: pc.localDescription,
-            dst: pc.remoteId
-          }));
-        })
-        .catch(ex => {
-          console.log('Recieve Offer error.', ex);
-        });
-    } else if (msg.type === 'ANSWER') {
-      console.log('%cRecieve answer', msg.ans);
-      pc.setRemoteDescription(new RTCSessionDescription(msg.ans))
-        .catch(ex => {
-          console.log('Recieve Answer error.', ex);
-        });
-    } else if (msg.type === 'CANDIDATE' && msg.cnd) {
-      console.log('%cRecieve candidate', 'color: red', msg.cnd);
-      pc.addIceCandidate(new RTCIceCandidate(msg.cnd))
-        .catch(ex => {
-          console.log('Recieve Candidate error.', ex);
-        });
-    } else if (msg.type === 'PING') {
-      socket.send(JSON.stringify({ type: 'PONG' }));
-    }
-  }
-}
-
-function pcSetup(remoteId) {
-  pc = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.skyway.io:3478' }] });
-  pc.remoteId = remoteId;
-  pc.onicecandidate = function (evt) {
-    console.log('%cpc onicecandidate', 'background: #79b74a; font-weight: bold; padding: 1px;');
-    socket.send(JSON.stringify({
-      type: 'CANDIDATE',
-      cnd: evt.candidate,
-      dst: this.remoteId
-    }));
-  }
-  pc.onnegotiationneeded = function (evt) {
-    console.log('%cpc onnegotiationneeded', 'background: #5d76a7; color: white; font-weight: bold; padding: 1px;');
-    var that = this;
-    that.createOffer()
-      .then(offer => {
-        return that.setLocalDescription(offer);
-      })
-      .then(_ => {
-        socket.send(JSON.stringify({
-          type: 'OFFER',
-          ofr: pc.localDescription,
-          dst: that.remoteId
-        }));
-      });
-  }
-  pc.onaddstream = function (evt) {
-    console.log('%cpc onaddstream', 'background: #ea4335, font-weight: bold; padding: 1px;');
-    remoteView.srcObject = evt.stream;
-  }
-  navigator.mediaDevices.getUserMedia({
+function webCamSetup(elm) {
+  return navigator.mediaDevices.getUserMedia({
     video: true,
     audio: false
   }).then(stream => {
-    selfView.srcObject = stream;
-    pc.addStream(stream);
+    elm.srcObject = stream;
+    return stream;
   }).catch(ex => console.log('getUserMedia error.', ex));
+}
+
+function callSetup(call) {
+  call.on('stream', stream => {
+    console.log('call on "stream"');
+    remoteView.srcObject = stream;
+  });
+  call.on('close', _ => {
+    console.log('call on "close"');
+  });
 }
