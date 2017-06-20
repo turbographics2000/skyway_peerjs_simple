144156fe39bc2dc88c7bb40b8b6f0b97d377e03d
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-20 14:30:49 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -35,7 +35,7 @@ function start() {
 }
 
 btnAddStream.onclick = evt => {
-  webCamSetup(selfView, devices[1]).then(stream => {
+  webCamSetup(selfView, videoDevices[1]).then(stream => {
     stream.getTracks().forEach(track => {
       call.localStream.addTrack(track);
     });
